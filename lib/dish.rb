66cc7e276f79d1ce9d6746e3e7f6b46b7266f39e
elliottm <m_elliott@live.com>
2013-10-27 21:56:18 +0000

    Name correction

---
+++ lib/dish.rb
@@ -0,0 +1,23 @@
+class Dish
+
+  def initialize price=nil
+    @price = price
+  end
+
+  attr_accessor :price
+
+  def has_price?
+    return true if @price != nil
+  end
+
+  def assign_price price
+    @price = price
+  end
+
+  def add_to_menu takeaway
+    takeaway.add_dish_to_menu self
+  end
+
+end
+
+
---
+++ spec/dish_spec.rb
@@ -0,0 +1,24 @@
+require 'dish'
+
+describe Dish do
+
+let(:dish) { Dish.new }
+  
+  it 'does not have a price' do
+  	expect(dish).not_to have_price
+  end
+
+  it 'has a price' do
+  	price = double :price
+  	dish.assign_price price
+  	expect(dish).to have_price
+  end
+
+  it 'can be added to takeaway list of dishes' do
+    #interaction test only, outcome takeaway class
+    takeaway = double :takeaway
+    expect(takeaway).to receive(:add_dish_to_menu)
+    dish.add_to_menu takeaway
+  end
+
+end
